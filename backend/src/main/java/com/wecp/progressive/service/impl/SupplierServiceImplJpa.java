3d6dac3a4c42fd41773b242b6de9afc0d0b91077
chaitanyamvr <chaitumvr@gmail.com>
2025-09-24 23:35:46 +0530

    Update SupplierServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
@@ -1,5 +1,37 @@
 package com.wecp.progressive.service.impl;
 
-public class SupplierServiceImplJpa  {
+import java.sql.SQLException;
+import java.util.List;
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.service.SupplierService;
+
+public class SupplierServiceImplJpa  implements SupplierService{
+
+    @Override
+    public int addSupplier(Supplier supplier)  {
+        // TODO Auto-generated method stub
+        return -1;
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers()  {
+        return List.of();
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliersSortedByName()  {
+        return List.of();
+    }
+    public void updateSupplier(Supplier supplier)
+    {
+
+    }
+    public void deleteSupplier(int supplierId)
+    {
+        
+    }
+    public Supplier getSupplierById(Supplier supplier) {
+       return null;
+    }
 
 }

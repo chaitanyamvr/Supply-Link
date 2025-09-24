daf4ee022f8f129acee29e4d8d8e38bade50e908
chaitanyamvr <chaitumvr@gmail.com>
2025-09-24 23:37:10 +0530

    Update WarehouseServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/WarehouseServiceImplJpa.java
@@ -1,5 +1,48 @@
 package com.wecp.progressive.service.impl;
 
-public class WarehouseServiceImplJpa  {
+import java.sql.SQLException;
+import java.util.List;
+
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.service.WarehouseService;
+
+public class WarehouseServiceImplJpa  implements WarehouseService{
+
+    @Override
+    public int addWarehouse(Warehouse warehouse) {
+        // TODO Auto-generated method stub
+        return -1;
+    }
+
+    @Override
+    public void deleteWarehouse(int warehouseId)  {
+       
+    }
+
+    @Override
+    public List<Warehouse> getAllWarehouses()  {
+        
+        return List.of();
+    }
+
+    @Override
+    public Warehouse getWarehouseById(int warehouseId)  {
+        return null;
+    }
+
+    @Override
+    public List<Warehouse> getWarehouseBySupplier(int supplierId) {
+       return List.of();
+    }
+
+    @Override
+    public List<Warehouse> getWarehousesSortedByCapacity() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public void updateWarehouse(Warehouse warehouse) throws SQLException {
+        
+    }
 
 }

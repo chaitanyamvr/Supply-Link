9b9d5a37202d0381a4bcf1d7775989c9bca8856e
chaitanyamvr <chaitumvr@gmail.com>
2025-09-26 22:04:18 +0530

    Add files via upload

---
+++ backend/src/main/java/com/wecp/progressive/controller/WarehouseController.java
@@ -0,0 +1,50 @@
+package com.wecp.progressive.controller;
+
+import com.wecp.progressive.entity.Warehouse;
+import com.wecp.progressive.service.impl.WarehouseServiceImplJpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/warehouse")
+public class WarehouseController {
+
+    @Autowired
+    WarehouseServiceImplJpa warehouseServiceImplJpa;
+    @GetMapping
+    public ResponseEntity<List<Warehouse>> getAllWarehouses() {
+        return null;
+    }
+    @GetMapping("/{warehouseId}")
+    public ResponseEntity<Warehouse> getWarehouseById(@PathVariable int warehouseId) {
+        return null;
+    }
+    @PostMapping
+    public ResponseEntity<Integer> addWarehouse(Warehouse warehouse) {
+        return new ResponseEntity<>(warehouseServiceImplJpa.addWarehouse(warehouse),HttpStatus.CREATED);
+    }
+    @PutMapping("/{warehouseId}")
+    public ResponseEntity<Void> updateWarehouse(int warehouseId, Warehouse warehouse) {
+        return null;
+    }
+
+    public ResponseEntity<Void> deleteWarehouse(int warehouseId) {
+        return null;
+    }
+
+    @GetMapping("/supplier/{supplierId}")
+    public ResponseEntity<List<Warehouse>> getWarehousesBySupplier( @PathVariable int supplierId) {
+        return new ResponseEntity<>(warehouseServiceImplJpa.getWarehouseBySupplier(supplierId),HttpStatus.OK);
+    }
+    
+
+}
---
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
@@ -0,0 +1,67 @@
+package com.wecp.progressive.service.impl;
+
+import java.sql.SQLException;
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wecp.progressive.entity.Product;
+import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.repository.SupplierRepository;
+import com.wecp.progressive.service.SupplierService;
+@Service
+public class SupplierServiceImplJpa  implements SupplierService{
+
+    @Autowired
+    SupplierRepository supplierRepository;
+    
+    public SupplierServiceImplJpa(SupplierRepository supplierRepository) {
+        this.supplierRepository = supplierRepository;
+    }
+
+    @Override
+    public int addSupplier(Supplier supplier) 
+    {
+        supplierRepository.save(supplier);
+        return supplier.getSupplierId();
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliers()  
+    {
+       return supplierRepository.findAll();
+    }
+
+    @Override
+    public List<Supplier> getAllSuppliersSortedByName()
+    {
+         List<Supplier> sortedSupplier=getAllSuppliers();
+        sortedSupplier.sort(Comparator.comparing(Supplier::getSupplierName));
+        return sortedSupplier;
+    }
+    public void updateSupplier(Supplier supplier) 
+    {
+       Supplier s=supplierRepository.findById(supplier.getSupplierId()).get();
+       if(s!=null)
+       {
+        s.setSupplierName(supplier.getSupplierName());
+        s.setUsername(supplier.getUsername());
+        s.setPassword(supplier.getPassword());
+        s.setEmail(supplier.getEmail());
+        s.setPhone(supplier.getPhone());
+        s.setRole(supplier.getRole());
+        supplierRepository.save(s);
+       }
+    }
+    public void deleteSupplier(int supplierId)
+    {
+        supplierRepository.deleteById(supplierId);
+    }
+    public Supplier getSupplierById(int supplierId)
+     {
+       return supplierRepository.findBySupplierId(supplierId);
+    }
+   
+}

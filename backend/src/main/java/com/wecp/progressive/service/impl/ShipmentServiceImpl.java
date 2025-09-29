60f4fe1f16f057ca5b27d9f7d6bffe687d44a1dd
chaitanyamvr <chaitumvr@gmail.com>
2025-09-29 23:17:40 +0530

    Update ShipmentServiceImpl.java

--- backend/src/main/java/com/wecp/progressive/service/impl/ShipmentServiceImpl.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/ShipmentServiceImpl.java
@@ -1,50 +1,58 @@
 package com.wecp.progressive.service.impl;
+ 
+import java.sql.SQLDataException;
 import java.sql.SQLException;
 import java.util.List;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+ 
 import com.wecp.progressive.entity.Shipment;
 import com.wecp.progressive.repository.ShipmentRepository;
 import com.wecp.progressive.service.ShipmentService;
-
+ 
 @Service
-public class ShipmentServiceImpl implements ShipmentService  {
-
+public class ShipmentServiceImpl implements ShipmentService{
     @Autowired
     ShipmentRepository shipmentRepository;
+ 
     @Override
-    public int addShipment(Shipment shipment) {
-       shipmentRepository.save(shipment);
-       return shipment.getShipmentId();
-    }
-
-    @Override
-    public void deleteShipment(int shipmentId) {
-      // shipmentRepository.deleteById( shipmentId);
-        
-    }
-
-    @Override
-    public List<Shipment> getAllShipments() {
+    public List<Shipment> getAllShipments()  {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getAllShipments'");
         return shipmentRepository.findAll();
     }
-
+ 
     @Override
-    public Shipment getShipmentById(int shipmentId) throws SQLException{
-
+    public Shipment getShipmentById(int shipmentId) throws SQLException {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'getShipmentById'");
+        // if(shipmentRepository.existsById(shipmentId)){
+        //     throw new SQLException("Not found");
+        // }
         return shipmentRepository.findByShipmentId(shipmentId);
     }
-
+ 
+    @Override
+    public int addShipment(Shipment shipment)  {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'addShipment'");
+        shipmentRepository.save(shipment);
+        return shipment.getShipmentId();
+    }
+ 
     @Override
     public void updateShipment(Shipment shipment) {
-        // Shipment s=shipmentRepository.findById(shipment.getShipmentId()).get();
-        // s.setDestinationLocation(shipment.getDestinationLocation());
-        // s.setExpectedDeliveryDate(shipment.getExpectedDeliveryDate());
-        // s.setShipmentDate(shipment.getShipmentDate());
-        // s.setProduct(shipment.getProduct());
-        // s.setSourceLocation(shipment.getSourceLocation());
-        // s.setStatus(shipment.getStatus());
-        // s.setWarehouse(shipment.getWarehouse());
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'updateShipment'");
+       // shipmentRepository.save(shipment);
     }
+ 
+    @Override
+    public void deleteShipment(int shipmentId) {
+        // TODO Auto-generated method stub
+        //throw new UnsupportedOperationException("Unimplemented method 'deleteShipment'");
+        //shipmentRepository.deleteBySupplierId(shipmentId);
+    }
+ 
+}

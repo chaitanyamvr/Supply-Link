7be442824843bc9784c302704806af7149886662
chaitanyamvr <chaitumvr@gmail.com>
2025-09-29 22:50:32 +0530

    Update ShipmentRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/ShipmentRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/ShipmentRepository.java
@@ -1,5 +1,11 @@
 package com.wecp.progressive.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.wecp.progressive.entity.Shipment;
 
-public interface ShipmentRepository {
-}
+public interface ShipmentRepository extends JpaRepository<Shipment,Integer> {
+    //@Query("Select s from Shipment s where s.shipmentDate=:shipmentDate")
+    Shipment findByShipmentId(int shipmentID);
+    //void deleteByWarehouseId(int warehouseId);
+    //void deleteBySupplierId(int supplierId);

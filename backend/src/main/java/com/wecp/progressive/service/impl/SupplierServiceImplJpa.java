5a5f784b57dfba4bd69f2975c4213538f31f6878
chaitanyamvr <chaitumvr@gmail.com>
2025-09-29 11:26:36 +0530

    Update SupplierServiceImplJpa.java

--- backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/SupplierServiceImplJpa.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.wecp.progressive.entity.Product;
 import com.wecp.progressive.entity.Supplier;
+import com.wecp.progressive.exception.SupplierAlreadyExistsException;
 import com.wecp.progressive.repository.SupplierRepository;
 import com.wecp.progressive.service.SupplierService;
 @Service
@@ -24,6 +25,14 @@ public class SupplierServiceImplJpa  implements SupplierService{
     @Override
     public int addSupplier(Supplier supplier) 
     {
+        if(supplierRepository.findByEmail(supplier.getEmail())!=null)
+        {
+         throw new SupplierAlreadyExistsException(null);
+        }
+        if(supplierRepository.findByUsername(supplier.getUsername())!=null)
+        {
+            throw new SupplierAlreadyExistsException(null);
+        }
         supplierRepository.save(supplier);
         return supplier.getSupplierId();
     }

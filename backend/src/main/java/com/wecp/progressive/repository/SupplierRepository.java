9b48128918793c354349ba8eee735ced154cb105
chaitanyamvr <chaitumvr@gmail.com>
2025-09-29 11:20:59 +0530

    Update SupplierRepository.java

--- backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
+++ backend/src/main/java/com/wecp/progressive/repository/SupplierRepository.java
@@ -12,5 +12,8 @@ public interface SupplierRepository extends JpaRepository<Supplier,Integer>{
    // @Query("Delete from Supplier s where s.supplierId=:supplierId")
    @Query("Select s from Supplier s where s.supplierId=:supplierId")
     Supplier findBySupplierId(@Param("supplierId") int supplierId);
+
+    Supplier findByUsername(String username);
+    Supplier findByEmail(String email);
     
 }

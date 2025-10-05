e7b9e9e8d87eba6130fab1eabcbbedee3b3672ad
chaitanyamvr <chaitumvr@gmail.com>
2025-10-05 22:52:37 +0530

    Add files via upload

---
+++ backend/src/main/java/com/wecp/progressive/exception/SupplierDoesNotExistException.java
@@ -0,0 +1,9 @@
+package com.wecp.progressive.exception;
+
+public class SupplierDoesNotExistException extends RuntimeException{
+
+    public SupplierDoesNotExistException(String message){
+        super(message);
+    }
+
+}

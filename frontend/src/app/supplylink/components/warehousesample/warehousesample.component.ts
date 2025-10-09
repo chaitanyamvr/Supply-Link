47b6de85a0aca8ffd5b6ec0133b1e8d035a4c3ab
chaitanyamvr <chaitumvr@gmail.com>
2025-10-09 22:57:40 +0530

    Add files via upload

---
+++ frontend/src/app/supplylink/components/warehousesample/warehousesample.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Warehouse } from '../../types/Warehouse';
+
+@Component({
+  selector: 'app-warehousesample',
+  standalone: true,
+  imports: [],
+  templateUrl: './warehousesample.component.html',
+  styleUrls: ['./warehousesample.component.css']
+})
+export class WarehouseSampleComponent {
+warehouse=new Warehouse(1,'12',"Flamingo",'Nevada',1000);
+}

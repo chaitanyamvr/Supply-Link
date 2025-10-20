ee1d956cd16f67034a0403a34eca336aa0fb32ef
chaitanyamvr <chaitumvr@gmail.com>
2025-10-20 16:52:06 +0530

    Delete backend directory

--- backend/src/main/java/com/wecp/progressive/exception/InsufficientCapacityException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class InsufficientCapacityException extends RuntimeException {
-    public InsufficientCapacityException(String msg) {
-        super(msg);
-    }
-}
--- backend/src/main/java/com/wecp/progressive/exception/SupplierDoesNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.wecp.progressive.exception;
-
-public class SupplierDoesNotExistException extends RuntimeException {
-    public SupplierDoesNotExistException(String msg) {
-        super(msg);
-    }
-}
